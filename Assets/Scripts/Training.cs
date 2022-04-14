6470e75baca4b016359d0d9709da7ed435aa6bde
RYOMA1205 <ryoma.take@gmail.com>
2022-04-14 22:21:55 +0900

    コミット　4月14日追加

---
+++ Assets/Scripts/Training.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Training : MonoBehaviour
+{
+    public string str;
+
+    int i;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    static void Main(string[] args)
+    {
+        int i = 11;
+        Console.WriteLine("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
